082dfd9a10089903cae2f448a0d09195638a995a
vishaldhayarkar2002 <97442543+vishaldhayarkar2002@users.noreply.github.com>
2025-01-21 12:15:39 +0530

    Update task15.py

--- task15.py
+++ task15.py
@@ -1,14 +1,14 @@
 def optimized_code(numbers):
     result = ""
-    max_num = max(numbers)  # Get the maximum number in the list
-    count_dict = {}  # Dictionary to store the counts of numbers
+    max_num = max(numbers)  # get max
+    count_dict = {}  # Dict to store count
 
     for num in numbers:
         if num % 2 == 0:
             square = num * num
             result += f"Square of {num} is {square}\n"
         
-        # Count occurrences of each number
+        # Count freq
         count_dict[num] = count_dict.get(num, 0) + 1
 
     result += f"\nMax number is {max_num}\n"
